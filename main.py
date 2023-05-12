cbb1239bda4fb2b65ff43c9f88b22f16330cdf5a
sreejaveeraghanta <sreeja.veeraghanta@outlook.com>
2023-05-11 17:16:30 -0700

    added a text similarity function

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, redirect, url_for, render_template
-from difflib import SequenceMatcher
+import functions
 
 app = Flask(__name__)
 
+# Home page route
 @app.route("/") 
 def home():
     return render_template("index.html")
 
+# File input page 
 @app.route("/fileInput")
 def fileInput():
     return render_template("fileInput.html")
+
+# Validating the file input from the file input page
 @app.route("/fileInput", methods=['POST'])
 def fileInputRead(): 
     file = request.form['file']
@@ -18,10 +22,12 @@ def fileInputRead():
     else: 
         return render_template("fileInput.html", errorFileInput="Invalid: please choose a file")
 
+# Double file input page
 @app.route("/fileVsfile")
 def fileVsfile():
     return render_template("fileVsfile.html")
 
+# Validating both files from the double file input page
 @app.route("/fileVsfile", methods=['POST'])
 def filesInputRead(): 
     file1 = request.form['file1']
@@ -31,10 +37,12 @@ def filesInputRead():
     else: 
         return render_template("fileVsfile.html", errorFilesInput="Invalid: please choose two files")
 
+# Text input page
 @app.route("/textInput")
 def textInput():
     return render_template("textInput.html")
 
+# Validating the text input
 @app.route("/textInput", methods=['POST'])
 def textInputRead(): 
     text = request.form['text']
@@ -43,19 +51,22 @@ def textInputRead():
     else: 
         return render_template("textInput.html", errorTextInput="Invalid: please type something")
 
+# Double text input page
 @app.route("/textVstext")
 def textVStext():
     return render_template("textVstext.html")
 
+# Validating both text inputs
 @app.route("/textVstext", methods=['POST'])
 def textsInputRead(): 
     text1 = request.form['text1']
     text2 = request.form['text2']
-    if (text1 != "" or text2 != ""):
-        return "The similarity is: "
+    if (text1 != "" and text2 != ""):
+        return "The similarity is: " + functions.checkSimilarity(text1, text2)
     else: 
         return render_template("textVstext.html", errorTextsInput="Invalid: please type something in both text fields")
 
+# Running the app
 if __name__ == "__main__":
     app.run()
 
